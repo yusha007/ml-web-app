652703083012c178a7b04a393e446945d662ca73
yusha007 <90753244+yusha007@users.noreply.github.com>
2022-07-22 20:58:44 +0530

    Add files via upload

---
+++ Health_Insurance1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 22 01:26:08 2022
+
+@author: yusha
+"""
+import streamlit as st
+import joblib
+
+
+def main():
+    html_temp="""
+    <div style="background-color:lightblue;padding:16px">
+    <h2 style="color:black";text-align:center>HEALTH INSURANCE COST PREDICTOR</h2>
+    </div>
+    """
+    
+    st.markdown(html_temp,unsafe_allow_html=True)
+    
+    # load the model
+    model = joblib.load('model_joblib_test')
+    
+    p1 = st.slider("Enter Your Age",18,90)
+    
+    s1=st.selectbox("Gender",("Male","Female"))
+    if s1=="Male":
+        p2=1
+    else:
+        p2=0
+
+    p3 =st.number_input("Enter Your BMI Value")
+    p4 = st.slider("Enter Number of Children",0,4) 
+    
+    s2=st.selectbox("Smoker",("Yes","No"))
+    if s2=="Yes":
+        p5=1
+    else:
+        p5=0
+        
+    p6 = st.slider("Enter Your Region [1-4]",1,4)
+    
+    if st.button('Predict'):
+        prediction = model.predict([[p1,p2,p3,p4,p5,p6]])
+        #st.balloons()
+        st.success('Insurance Amount is {} '.format(round(prediction[0],2)))    
+    
+if __name__ == '__main__':
+    main()
